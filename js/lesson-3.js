98cba8d2efc44a1f5b1b4975cf003e184a599a13
Stepannovv <stepannovv@gmail.com>
2024-06-01 15:58:27 +0200

    3st

--- js/lesson-3.js
+++ js/lesson-3.js
@@ -138,23 +138,101 @@ const users = [
 // console.log(getSortedUniqueSkills(users));
 
 //Створити статистику - об'єкт, у якому вказується кількість тегів
-const tweets = [
-  { id: "000", likes: 5, tags: ["js", "nodejs"] },
-  { id: "001", likes: 2, tags: ["html", "css"] },
-  { id: "002", likes: 17, tags: ["html", "js", "nodejs"] },
-  { id: "003", likes: 8, tags: ["css", "react"] },
-  { id: "004", likes: 0, tags: ["js", "nodejs", "react"] },
-];
+// const tweets = [
+//   { id: "000", likes: 5, tags: ["js", "nodejs"] },
+//   { id: "001", likes: 2, tags: ["html", "css"] },
+//   { id: "002", likes: 17, tags: ["html", "js", "nodejs"] },
+//   { id: "003", likes: 8, tags: ["css", "react"] },
+//   { id: "004", likes: 0, tags: ["js", "nodejs", "react"] },
+// ];
 
-const newTweets = tweets.flatMap((tweet) => tweet.tags);
-const statistic = {};
-newTweets.forEach((tag) => {
-  console.log(statistic[tag]);
-  if (!statistic[tag]) {
-    statistic[tag] = 1;
-  } else {
-    statistic[tag] += 1;
-  }
+// const newTweets = tweets.flatMap((tweet) => tweet.tags);
+// const statistic = {};
+// newTweets.forEach((tag) => {
+//   console.log(statistic[tag]);
+//   if (!statistic[tag]) {
+//     statistic[tag] = 1;
+//   } else {
+//     statistic[tag] += 1;
+//   }
 
-  console.log(statistic);
-});
+//   console.log(statistic);
+// });
+//2. Напиши класс Client який створює об'єкт
+//із властивостями login, email
+//Об'яви приватні властивості #login і #email,
+//доступ до яких зроби через геттер и сеттер:
+// get getClientData() має повертати об'єкт з переліченими властивостями
+// set changeEmail(newEmail) перезаписує пошту користувача
+// class Client {
+//   #login;
+//   #email;
+//   constructor(login, email) {
+//     this.#login = login;
+//     this.#email = email;
+//   }
+//   get getClientData() {
+//     return { clientLogin: this.#login, clientEmail: this.#email };
+//   }
+//   set changeEmail(newEmail) {
+//     this.#email = newEmail;
+//   }
+// }
+// const client = new Client("kolokol", "ewa@gmail.com");
+// console.log((client.changeEmail = "waw@gmail.com"));
+
+//3. Напиши класс Notes який управляє коллекцієй нотаток у
+//властивості items.
+//Нотатка це  об'єкт з властивостями text, priority
+//Додай класу статичну властивість Priority,
+//в якій буде зберігатись об'єкт з пріорітетами ("hight", "middle", "low").
+//Додай методи addNote(note), removeNote(noteText)
+//updatePriority(noteText, newPriority),
+// class Notes {
+//   static Priority = { HIGHT: "hight", MIDDLE: "middle", LOW: "low" };
+//   constructor() {
+//     this.items = [];
+//   }
+//   addNote(note) {
+//     this.items.push(note);
+//   }
+//   removeNote(noteText) {
+//     this.items = this.items.filter((item) => item.text !== noteText);
+//   }
+//   updatePriority(noteText, newPriority) {
+//     const result = this.items.find((item) => item.text === noteText);
+//     if (result) {
+//       result.priority = newPriority;
+//     }
+//   }
+// }
+// const notes = new Notes();
+// notes.addNote({ text: "ofofofo", priority: Notes.Priority.HIGHT });
+// notes.addNote({ text: "fikoloj", priority: Notes.Priority.MIDDLE });
+// notes.addNote({ text: "clientjfsdi", priority: Notes.Priority.LOW });
+// notes.removeNote("fikoloj");
+// notes.updatePriority("ofofofo", Notes.Priority.LOW);
+// console.log(notes);
+
+// ********************* HOMEWORK ****************************
+// 4. Створити клас Worker, у якого є властивості name, age, salary.
+//У класу Worker є метод getSalary, який повертає повідомлення
+//"Worker <name> has salary <salary> dollars"
+//Створити клас WorkerPosition, у якого є властивість position
+//і який успадковує клас Worker, додаючи метод getPosition
+// який повертає повідомлення "<name> works as <position>"
+
+// Task 10
+
+// Знайти найдорожчий продукт у категорії "meat"
+// Напиши для цього функцію getMostExpensiveMeatProduct(products)
+// const products = [
+//   { name: 'Apple', category: 'fruits', price: 1.2, stock: 50 },
+//   { name: 'Banana', category: 'fruits', price: 0.8, stock: 100 },
+//   { name: 'Carrot', category: 'vegetables', price: 0.5, stock: 200 },
+//   { name: 'Broccoli', category: 'vegetables', price: 1.0, stock: 150 },
+//   { name: 'Milk', category: 'dairy', price: 1.5, stock: 20 },
+//   { name: 'Cheese', category: 'dairy', price: 2.5, stock: 5 },
+//   { name: 'Chicken', category: 'meat', price: 5.0, stock: 30 },
+//   { name: 'Beef', category: 'meat', price: 7.0, stock: 25 }
+// ];
