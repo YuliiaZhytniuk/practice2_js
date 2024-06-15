5c4aee6b1c37865245144a7386d07b2fee7852ec
Stepannovv <stepannovv@gmail.com>
2024-06-15 16:05:52 +0200

    task 4.4

--- js/lesson-4.js
+++ js/lesson-4.js
@@ -96,24 +96,80 @@
 //   items.forEach((item) => (item.textContent *= 2));
 // }
 
-
 // Завдання 16
 //  При натисканні на будь-який рядок у табличці відобразіть
 //  повідомлення з назвою продукту та його ціною.
 //  "Ви вибрали <product> за <price>".
 
-const table = document.getElementById('productTable');
+// const table = document.getElementById('productTable');
 
-const text = document.getElementById('productDetails');
+// const text = document.getElementById('productDetails');
 
-table.addEventListener('click', showText);
+// table.addEventListener('click', showText);
 
-function showText(event) {
-    if (event.target.nodeName !== 'TD') {
-        return
-    } 
-    const parent = event.target.parentNode;
-    const product = parent.firstElementChild.textContent;
-    const price = parent.lastElementChild.textContent;
-    text.insertAdjacentHTML('afterbegin', `<p>Ви вибрали ${product} за ${price}</p>`);    
+// function showText(event) {
+//     if (event.target.nodeName !== 'TD') {
+//         return
+//     }
+//     const parent = event.target.parentNode;
+//     const product = parent.firstElementChild.textContent;
+//     const price = parent.lastElementChild.textContent;
+//     text.insertAdjacentHTML('afterbegin', `<p>Ви вибрали ${product} за ${price}</p>`);
+// }
+
+/*
+Завдання 8
+При натисканні на кожну з кнопок підсумовуються значення з data-атрибутів.
+За натисканням на кнопку "Вивести результат" виводиться сума значення, а також статистика з
+інформацією про те, яка кнопка була натиснута скільки разів.
+*/
+const list = document.querySelector(".statList");
+const result = document.getElementById("resultButton");
+const resultMessage = document.getElementById("resultSection");
+let resultData = 0;
+list.addEventListener("click", oneBtnClick);
+function oneBtnClick(event) {
+  if (event.target.nodeName !== "BUTTON") {
+    return;
+  }
+  const value = Number(event.target.dataset.number);
+  resultData += value;
+  console.log(resultData);
 }
+result.addEventListener("click", showResult);
+function showResult() {
+  resultMessage.textContent = `Загальна кількість ${resultData}`;
+  resultData = 0;
+}
+//  **************** HOMEWORK *************
+
+// / Створити червоний квадрат розміром 50 на 50 рх
+// Додати кнопку "Зменшити", яка робить квадрат менше на 10 пікселів
+// Додати кнопку "Збільшити", яка робить його більше на 10 пікселів.
+//     <!-- ЗАДАЧА 3 -->
+//     <!-- <div>
+//       <p class="taskTitle">ЗАДАЧА 3</p>
+//       <input id="passwordInput" type="text" />
+//       <button id="passwordButton">Приховати</button>
+//     </div> -->
+/*
+Завдання 3
+Кнопка "Приховати" ховає текст і замінює назву кнопки на
+"Розкрити", при повторному натисканні текст знову стає доступним
+і кнопка набуває початкового вигляду.
+*/
+// <!-- ЗАДАЧА 11 -->
+//     <!-- <p class="taskTitle">ЗАДАЧА 11</p>
+//     <input class="contactsFilter" type="text" />
+//     <ul class="contacts">
+//       <li class="contact">Peter Miller</li>
+//       <li class="contact">John Carter</li>
+//       <li class="contact">Amanda Diaz</li>
+//       <li class="contact">Brad Sanders</li>
+//       <li class="contact">Devis Brooks</li>
+//       <li class="contact">Robert Morgan</li>
+//     </ul> -->
+/*
+Завдання 11
+Наведено список людей. Зроби фільтр на ім'я.
+*/
